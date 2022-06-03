1db6d0905cc3c78be6634945de9f30276b917ffa
chanhan2 <chanhan2@github.com>
2022-06-03 01:02:10 -0400

    Refractored the function flatten() in Python/flatten.py to just use syntactic notation instead (cause it's already built in for Python), got rid of the last if-statement as it was useless/pointless as I was thinking poorly, and got rid of the accum parameter since it now takes advantage of the built in syntactic notation

--- Python/flatten.py
+++ Python/flatten.py
@@ -1,19 +1,13 @@
-def myAppend(lst, elem):
-    lst.append(elem)
-    return lst
-
-def flatten(lst, accum):
+def flatten(lst):
     if not lst:
-        return accum
+        return []
     if not isinstance(lst[0], list):
-        return flatten(lst[1:], myAppend(accum, lst[0]))
-    if isinstance(lst[0], list):
-        accum = flatten(lst[0], accum)
-    return flatten(lst[1:], accum)
+        return [lst[0]] + flatten(lst[1:])
+    return flatten(lst[0]) + flatten(lst[1:])
 
 if __name__ == '__main__':
     lst = [[1,2,3,4,[[5],[3,-2,[6,[1,[11,2,123,[127,[1],7],43]],110]]],[-1,[[[[3]]]]]],6,[11],7,8,9,10]
     print(lst[1:])
-    result = flatten(lst, [])
+    result = flatten(lst)
     print(result)
     print(len(result))
